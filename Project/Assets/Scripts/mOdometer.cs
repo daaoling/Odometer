73c6d460a1cd84c620852c6047c3c5547de8ed2f
daaoling <1294246096@qq.com>
2015-12-10 23:25:48 +0800

    nothing

--- Project/Assets/Scripts/mOdometer.cs
+++ Project/Assets/Scripts/mOdometer.cs
@@ -3,13 +3,46 @@ using System.Collections;
 
 public class mOdometer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private int _curValue = 0;
+	public int curValue
+	{
+		get{ return this.curValue;}
+		set{ 
+			this._curValue = value;
+
+			OnCurValueChanged();
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Start()
+	{
+
+	}
+
+	public int incrementValue;
+	public float delay;
+	public void AddValue(int incrementValue)
+	{
+		this.incrementValue = incrementValue;
+		this.delay = 1.0f / this.incrementValue;
+		StartCoroutine(InternalIncrementSmooth());
+	}
+
+
+
+
+	public IEnumerator InternalIncrementSmooth()
+	{
+		for (int i = 0; i < this.incrementValue; i++) 
+		{
+			this.curValue += 1;
+			yield return new WaitForSeconds(this.delay);
+		}
+	}
+
+	public UILabel curLabel;
+	public void OnCurValueChanged()
+	{
+		this.curLabel.text = this.curValue.ToString();
 	}
 }
--- Project/Assets/mDemo/mTest.cs
+++ Project/Assets/mDemo/mTest.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class mTest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public mOdometer odometer;
+
+	void OnGUI() {
+		if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button"))
+		{
+			odometer.AddValue(5);
+		}
 	}
 }
